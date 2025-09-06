7436c5e946e3d8ddfa8c0c7c0d76a6439be25fcd
manas8938 <anakxofficial@gmail.com>
2025-09-06 08:20:07 +0500

    Deploy Flutter web (base-href /portfolio/)

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "920148838"
+    serviceWorkerVersion: "3628714529"
   }
 });
